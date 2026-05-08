cf4e0f725958d7eee2b2bacdd4a70ecd72f76d2e
BaBajb <sixtencamen@icloud.com>
2026-05-08 22:17:19 +0200

    Typ klar

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
-function submit() {
-    alert("hej på dig")
+function handleSubmit(event) {
+    event.preventDefault();
+    
+    getUser = document.querySelector("input").value;
+    getText = document.querySelector("textarea").value;
+
+    
+    div = document.createElement("div");
+    h2 = document.createElement("h2");
+    p = document.createElement("p");
+    
+    h2.textContent = getUser;
+    p.textContent = getText;
+    
+    div.classList.add("forumform");
+    div.classList.add("kommetar");
+    
+    div.appendChild(h2);
+    div.appendChild(p);
+    
+    document.querySelector(".kommentarer").appendChild(div);
+    
+    document.querySelector("textarea").value = "";
 }
